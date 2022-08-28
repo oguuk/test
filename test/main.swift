ddfdb3c5b3a727fe21fe3bb029d73cc45dbe98da
oguuk <ogw135@gmail.com>
2022-08-28 14:49:54 +0900

    Initial Commit

---
+++ test/main.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+var num = Int(readLine()!)!
+
+while num != 0 {
+    
+    let string = readLine()!.components(separatedBy: " ").map { Int(String($0))! }
+    
+    var houses = Array(repeating: <#T##_#>, count: <#T##Int#>)
+    
+    num -= 1
+}
