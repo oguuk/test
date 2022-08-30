8e05fe2373eb0179d71eddd0f852b1549f090379
oguuk <ogw135@gmail.com>
2022-08-30 11:42:08 +0900

    Make "This is a test" String

--- test/main.swift
+++ test/main.swift
@@ -1,12 +1,3 @@
 import Foundation
 
-var num = Int(readLine()!)!
-
-while num != 0 {
-    
-    let string = readLine()!.components(separatedBy: " ").map { Int(String($0))! }
-    
-    var houses = Array(repeating: <#T##_#>, count: <#T##Int#>)
-    
-    num -= 1
-}
+let xcode = "This is a test"
